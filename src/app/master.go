d4b51f1c87a303efeaf0f5ebafc1501f9fcbd535
baihai <57270348@qq.com>
2017-03-06 16:48:43 +0800

    using redirect

--- src/app/master.go
+++ src/app/master.go
@@ -5,7 +5,7 @@ import (
     "fmt"
     "net/http"
     "time"
-    "net/url"
+    _ "net/url"
 )
 
 var fsHandler = http.FileServer(http.Dir("resfolder"))
@@ -19,18 +19,21 @@ func main() {
     //~ 精确匹配
     http.HandleFunc("/version.php", func(w http.ResponseWriter, r *http.Request){
         fmt.Printf("%s: request for version profile.\n", time.Now())
+        /*
         r2 := new(http.Request)
         *r2 = *r // Copy
         r2.URL = new(url.URL)
         *r2.URL = *r.URL
         r2.URL.Path = "version.txt"
         fsHandler.ServeHTTP(w, r2)
+        */
+        http.Redirect(w, r, "/resfolder/version.txt", http.StatusMovedPermanently)
     })
     //~ 默认匹配
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-        fmt.Printf("In <%s>\n", r.URL.Path)
+        //fmt.Printf("In <%s>\n", r.URL.Path)
         stripperHandler.ServeHTTP(w, r)
     })
-    http.ListenAndServe(":8099", nil)
+    http.ListenAndServe(":12000", nil)
 }
 
